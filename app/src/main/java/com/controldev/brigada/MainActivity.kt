b8231402a0bde8ad6876ee1bd90b65a742e5cb31
NicolasOteiza <oteizanicolas@gmail.com>
2021-06-15 20:23:21 -0400

    first-commit

---
+++ app/src/main/java/com/controldev/brigada/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.controldev.brigada
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
